b2e6c4cadfe9b5b6295aa423358f3bb79d8f542c
Christian Payne <67926222+ChrisPayneHome@users.noreply.github.com>
2023-02-04 23:50:44 +0000

    Adding new function definitions

--- scrapers.py
+++ scrapers.py
@@ -3,46 +3,95 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import time
+import random
 
-# define function to retieve actors list
-def retrievePage(url) :
+
+
+# define function to retieve actors
+def retrieveCast(url) :
     try:
         page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        name = soup.find("h3", {"itemprop":"name"}).text
-        name = re.sub("\s\s+", " ", name).strip("\n")
-        if re.search("[(\[][0-9]{4}[)\]]", name):
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, "html.parser")
+            name = soup.find("h3", {"itemprop":"name"}).text
+            name = re.sub("\s\s+", " ", name).strip("\n")
             print("Scraping cast list from: %s" % name)
-            # check if there is a cast list for the url
+                # check if there is a cast list for the url
             if str(soup.find("div", {"no_content":"list"})) != "None":
                 raise Exception("No cast list for this film")
-            else:
+            elif str(soup.find('table', {"class":"cast_list"})) != "None":
                 html = soup.find('table', {"class":"cast_list"})
                 cast_table = pd.read_html(str(html))
                 cast_list = list(pd.DataFrame(cast_table[0]).iloc[:, 1])
+            else:
+                print(soup.find('table', {"class":"cast_list"}))
             return(cast_list)
         else:
-            print("Error: not a Movie")
-    except Exception as e:
-        print("Error: %s" % e)
+            print("Status code %i, can't connect to: %s" % (page.status_code, url))
+            if page.status_code == 404:
+                print("Can't retrieve page: status code %i" % page.status_code)
+            else:
+                print("Retrying")
+                time.sleep(random.randint(1, 6))
+                retrieveCast(url)
+    except (KeyError, NameError, TypeError, AttributeError, IndexError) as e:
+        print("Error: Can't retrieve cast list (%s)" % e)
 
 
 
 # define function to update actor_table
-def filterList(actor_list, actor_table):
-    for actor in actor_list:
-        for link in actor_list:
-            # if actor and link are the same or invalid
-            if actor in [np.nan, "nan", link, "Rest of cast listed alphabetically:"]:
-                continue
-            # ...else add the row to the table
-            else:
-                if link in [np.nan, "nan", "Rest of cast listed alphabetically:"]:
+def filterList(actor_list):
+    add_rows = []
+    try:
+        for actor in actor_list:
+            for link in actor_list:
+                # if actor and link are the same or invalid
+                if actor in [np.nan, "nan", link, "Rest of cast listed alphabetically:"]:
                     continue
+                # ...else add the row to the table
                 else:
-                    new_row = [actor, link]
-                    actor_table.loc[len(actor_table)] = new_row
-    return(actor_table)
+                    if link in [np.nan, "nan", "Rest of cast listed alphabetically:"]:
+                        continue
+                    else:
+                        add_rows.append(pd.DataFrame({'Actor1':[actor], 'Actor2':[link]}))
+        new_rows = pd.concat(add_rows)
+        return(new_rows)
+    except Exception as e:
+        print("Error: Can't retrieve actor table (%s)" % e)
+
+
+
+# define combined function to retrieve actors and table
+def combinedRetrieve(url, dataframes):
+    cast_list = retrieveCast(url)
+    new_rows = filterList(cast_list)
+    dataframes.append(new_rows)
+
+
+
+# define function to retrieve sample urls with a number input
+def getURLs():
+    # define empty url list
+    sample_urls = []
+    # begin loop of range
+    for i in range(1, 5000, 50):
+        print("Scraping movies: %i - %i" % (i, i + 50))
+        try:
+            sample_url = "https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&view=simple&sort=user_rating,desc&start="+ str(i) +"&ref_=adv_nxt"
+            page = requests.get(sample_url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            movie_table = soup.findAll("div", {"class":"lister-item mode-simple"})
+            # begin loop of movies within range
+            for j in range(0, len(movie_table)):
+                # find the 7 digit id number for each film entry
+                movie_id = re.findall(r'[0-9]{7}', str(movie_table[j]))[0]
+                movie_url = "https://www.imdb.com/title/tt" + movie_id + "/fullcredits?ref_=tt_cl_sm"
+                sample_urls.append(movie_url)
+        except Exception as e:
+            break
+    return(sample_urls)
+
 
 
 
